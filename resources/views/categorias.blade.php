b3cae2b3a3e4c7cc4786200e1e4d898a6dc17640
DiegoB <diegobaesv@gmail.com>
2025-03-20 22:27:52 -0500

    Update categorias.blade.php

--- resources/views/categorias.blade.php
+++ resources/views/categorias.blade.php
@@ -6,55 +6,14 @@
 
     <div class="flex flex-wrap justify-evenly mt-4 gap-4">
 
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
+        @foreach($categorias as $categoria)
 
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
+            <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
+                <img class="h-[130px] w-full object-cover rounded" src="{{ $categoria->imagen_url }}" alt="">
+                <p class="text-center mt-3 font-medium text-gray-800">{{ $categoria->nombre }}</p>
+            </div>
 
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
-
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
-
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
-
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
-
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
-
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
-
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
-
-        <div class="bg-gray-200 hover:bg-gray-300 w-[300px] h-[200px] p-4 rounded cursor-pointer">
-            <img class="h-[130px] w-full object-cover rounded" src="https://th.bing.com/th/id/R.2119f7e852f9a3d92d802f87550abc53?rik=WelY6Ye8U6Pd6A&pid=ImgRaw&r=0" alt="">
-            <p class="text-center mt-3 font-medium text-gray-800">Categoria 1</p>
-        </div>
+        @endforeach
 
     </div>
 
